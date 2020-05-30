b8e1c9186f58db39912e3ce83fe68d1fbcd853b1
SillyFreak <sillyfreak.ck@gmail.com>
2020-05-30 10:55:58 +0200

    use a lookup table of steps instead of calculating deltas from the current absolute value

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use anyhow::{Context, Result};
 
 const BRIGHTNESS_FILE: &'static str = "/sys/class/backlight/intel_backlight/brightness";
 
+const SMART_STEPS: &'static [u32] = &[
+    0, 1, 100, // low end
+    200, 300, 400, // 100 steps
+    600, 800, 1000, 1200, 1400, 1600, 1800, 2000, // 200 steps
+    3000, 4000, 5000, 6000, // 1000 steps
+    8000, 10000, 12000, 14000, 16000, 18000, 20000, // 2000 steps
+    25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, // 5000 steps
+];
+
 fn usage() -> ! {
     eprintln!("Usage: xhacklight [=N|+N|-N|inc|dec]");
     process::exit(1);
@@ -20,7 +29,11 @@ fn get_brightness() -> Result<u32> {
 }
 
 fn set_brightness(brightness: u32) -> Result<()> {
-    let brightness = if brightness <= 60000 { brightness } else { 60000 };
+    let brightness = if brightness <= 60000 {
+        brightness
+    } else {
+        60000
+    };
     let brightness = brightness.to_string();
     fs::write(BRIGHTNESS_FILE, brightness)
         .context(format!("could not write file {}", BRIGHTNESS_FILE))?;
@@ -49,22 +62,26 @@ fn adjust_brightness(adjustment: Adjustment) -> Result<()> {
             Adjustment::Inc(change) => brightness.saturating_add(change),
             Adjustment::Dec(change) => brightness.saturating_sub(change),
             Adjustment::SmartInc => {
-                let change = match brightness {
-                    0..=199 => 50,
-                    200..=1999 => 200,
-                    2000..=19999 => 2000,
-                    _ => 5000,
-                };
-                brightness.saturating_add(change)
+                let index = SMART_STEPS.binary_search(&brightness);
+                let index = index.map_or_else(
+                    // the insert index is to the right of the current value
+                    |insert_index| insert_index,
+                    // take the index of the value to the right
+                    |index| index + 1,
+                );
+                // make sure that the index is not out of bounds
+                let index = std::cmp::min(index, SMART_STEPS.len() - 1);
+                SMART_STEPS[index]
             }
             Adjustment::SmartDec => {
-                let change = match brightness {
-                    0..=200 => 50,
-                    201..=2000 => 200,
-                    2001..=20000 => 2000,
-                    _ => 5000,
-                };
-                brightness.saturating_sub(change)
+                let index = SMART_STEPS.binary_search(&brightness);
+                let index = index.map_or_else(
+                    // the insert index is to the right of the current value; go one to the left
+                    |insert_index| insert_index.saturating_sub(1),
+                    // take the index of the value to the left
+                    |index| index.saturating_sub(1),
+                );
+                SMART_STEPS[index]
             }
         }
     };
